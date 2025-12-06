ceae9b0ce824a416be0e0395fb60f7500b25dbb9
Dacosmicgiant <vedantvanpro@gmail.com>
2025-12-06 11:56:04 +0530

    modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://github.com/Dacosmicgiant/nexcore_taskmanager.com',
+  base: '/nexcore_taskmanager/',
 })
